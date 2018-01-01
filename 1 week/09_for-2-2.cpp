440878b08651d61b70f81dc3d93ccaad20aa14e2
karlian <inis.karlian@gmail.com>
2018-01-02 00:54:11 +0900

    ubuntu C++ 스터디 1주차 과제

--- 1 week/09_for-2-2.cpp
+++ 1 week/09_for-2-2.cpp
@@ -1,26 +1,30 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-#define MAX_LINE 7
-
 int main() {
+ 
 
-  // 과제-2(1) : 아래 예시 처럼 A 모양 출력하기
-  cout << "semple" << endl << endl; 
-  cout << "      *\n" << "     * *\n" << "    *   *\n" << "   *     *\n" << "  *********\n" << " *         *\n" << "*           *\n" << endl << endl;
+  //과제-2(2) : 사이즈를 입력받아 크기를 조절할 수 있는 A 모양 만들기
+ 
+  cout << "\n\n사이즈 입력받는 A 모양 출력하기" << endl << endl; 
 
-  cout << "A 모양 출력하기" << endl << endl; 
+  int line_num = 7;
 
-  int line_space = MAX_LINE -1 ;
+  cout << "A모양 세로 줄 수 : "; 
+  cin >> line_num;
+  cout << endl; 
+  
+  int line_space = line_num -1 ;
   int mid_space = 0;
-  int mid_line = ((double)4 / 7) *  MAX_LINE;
+  int mid_line = ((double)4 / 7) *  line_num;
 
-  for (int i = 0; i < MAX_LINE; i++) {
+
+  for (int i = 0; i < line_num; i++) {
 
     mid_space = (i * 2) - 1;
 
-    for (int j = 0; j < (MAX_LINE * 2) ; j++) {
+    for (int j = 0; j < (line_num * 2) ; j++) {
       
       if(j == line_space){  // 앞쪽 *
         cout << "*";
@@ -36,7 +40,7 @@ int main() {
         if(mid_space > 0){    
 
           if(i == mid_line){
-            cout << "*";      // 중앙 막대부분 *
+            cout << "*";      // 중앙 막대부분 
             mid_space--;
           }else{
             cout << " ";      // 중앙 공백처리
@@ -53,7 +57,6 @@ int main() {
     
     line_space--;
   }
-
 
   return 0;
 }        
