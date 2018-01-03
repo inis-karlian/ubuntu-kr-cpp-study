68c78587e85596e0286940cc61a48bff093285d0
inis-karlian <inis.karlian@gmail.com>
2018-01-03 20:13:49 +0900

    ubuntu C++ 스터디 2주차 학습 내용

---
+++ 2 week/10_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+    array<int, 10> arr = {1, 2, 3, 4, 5};
+
+    auto it = arr.begin();
+
+    while (it != arr.end()) {
+        cout << *it << endl;
+        it++;
+    }
+
+    return 0;
+}
---
+++ 2 week/11_struct.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+//#include <string>
+
+using namespace std;
+#define size 64
+struct make_item
+{
+    char name[size];
+    char country[size];
+    int price;
+    char os[size];
+};
+
+int main() {
+    
+    int country_n = 0;
+    make_item item;
+    
+    strcpy(item.name, "good-laptop");
+
+    cout << "enter your country : ";
+    cin >> item.country;
+
+    strcat(item.name, item.country);
+
+    item.price = 700000;
+
+   
+    cout << "name : " <<"-"<< item.name << endl;
+    cout << "price : " << item.price << endl;
+    cout << "os : "<< endl; // os pre-install = en, os none = ko
+
+    return 0;
+}
